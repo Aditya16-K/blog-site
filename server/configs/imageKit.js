962fc227513e003865a9d04e11ac8b49b7cfe54c
Aditya16-K <golukumar4288513@gmail.com>
2025-08-16 23:23:27 +0545

    Fix: ImageKit initialization for Railway deployment

--- server/configs/imageKit.js
+++ server/configs/imageKit.js
@@ -1,7 +1,7 @@
 import ImageKit from "imagekit";
 
 var imagekit = new ImageKit({
-  publicKey: process.env.IMAGEkIT_PUBLIC_KEY,
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
   privateKey: process.env.IMAGEkIT_PRIVATE_KEY,
   urlEndpoint: process.env.IMAGEkIT_URL_ENDPOINT,
 });
